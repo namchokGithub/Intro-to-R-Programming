330a57f155622ea561cf2d6337512cc561e15670
namchokGithub <pech4751@gmail.com>
2019-12-31 08:45:03 +0700

    Create list

---
+++ ex06.R
@@ -0,0 +1,17 @@
+#######################
+####     Ex06      ####
+#######################
+
+# Data Structure
+# 1. Vector
+# 2. List
+# 3. Matirx
+# 4. Data frame
+
+# Data frame
+
+# Create list
+cities <- c("BKK", "TOKYO", "LONDON")
+countries <- c("TH", "JPN", "UK")
+population <- c(10000, 20000, 34000)
+
