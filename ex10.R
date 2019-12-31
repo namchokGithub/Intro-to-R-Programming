63d78f72956c219fa95f4ac41252940ffe2c6e00
namchokGithub <pech4751@gmail.com>
2019-12-31 14:09:32 +0700

    While loop

---
+++ ex10.R
@@ -0,0 +1,25 @@
+# CONTROL FLOW
+    # IF
+    # FOR .. LOOP
+    # WHILE .. LOOP
+
+# WHILE .. LOOP
+
+x <- 1
+# important, initialize starting point
+
+while (x <= 10) {
+    print(x)
+    x <- x+1
+}
+
+# add string
+a = "Namchok"
+b = "Singhachai"
+paste(a, b)
+
+x <- 1
+while (x <= 10) {
+    print(paste(x, "less than or equal to ten"))
+    x <- x+1
+}
