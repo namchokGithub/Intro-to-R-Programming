2e5278f4de3e49886f70c5b7dd5f62348a7809db
namchokGithub <pech4751@gmail.com>
2019-12-31 13:51:14 +0700

    if else

---
+++ ex08.R
@@ -0,0 +1,22 @@
+# CONTROL FLOW
+    # IF
+    # FOR .. LOOP
+    # WHILE .. LOOP
+
+# IF
+
+X <- 9 
+
+if(x %% 2 == 0) {
+    print("Even number")
+} else {
+    print("Odd number")
+}
+
+oddOrEven <- function(x) {
+    if(x %% 2 == 0) {
+        print("Even number")
+    } else {
+        print("Odd number")
+    }
+}
