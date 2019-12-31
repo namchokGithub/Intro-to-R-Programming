f2661efc42cdef2b14e6ce49a0e4018f60496d71
namchokGithub <pech4751@gmail.com>
2019-12-31 12:14:15 +0700

    Useful function

--- ex07.R
+++ ex07.R
@@ -7,3 +7,22 @@ sum(x)
 median(x)
 sd(x)
 var(x)
+
+# Function help
+help(mean)
+
+# Create list
+yy <- c(1:10, NA, 20, 24)
+
+# Find mean but found # NA
+mean(yy)
+
+# Use na.rm = TRUE
+mean(yy, na.rm = TRUE)
+
+# Use five number to find minimum, lower-hinge, 
+# median, upper-hinge, maximum
+help("fivenum")
+
+y <- 1:1000
+fivenum(y)
